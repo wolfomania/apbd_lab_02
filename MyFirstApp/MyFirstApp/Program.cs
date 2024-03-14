d7286eea5b084cd21ee0c4923889e48cc424b527
wolfomania <kulyzhskyi.andrii@gmail.com>
2024-03-14 17:28:42 +0100

    Add method max

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -16,6 +16,19 @@ static double GetAverage(int[] numbers)
     return sum / numbers.Length;
 }
 
+static int GetMax(int[] numbers)
+{
+    int max = 0;
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        max = numbers[max] < numbers[i] ? i : max;
+    }
+
+    return max;
+}
+
 int[] numbers = new[] { 1, 3, 5, 12, 65, 3, 5 };
 double var = GetAverage(numbers);
 Console.WriteLine(var);
+
+Console.WriteLine(GetMax(numbers));
